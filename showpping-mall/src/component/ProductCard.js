ec750d056a23b8f24a2b285820dcb6d770ff67a8
wonjin <wonjine7@naver.com>
2022-09-29 15:26:15 +0900

    API 부르기

---
+++ showpping-mall/src/component/ProductCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src='https://lp2.hm.com/hmgoepprod?set=source[/51/02/510297193b1fc0823baee7b75c123415ea6efda3.jpg],origin[dam],category[],type[LOOKBOOK],res[w],hmver[1]&call=url[file:/product/main]'/>
+      <div>Conscious choice</div>
+      <div>벨티드 트위 코트</div>
+      <div>금액</div>
+    </div>
+  )
+}
+
+export default ProductCard
--- showpping-mall/src/page/ProductAll.js
+++ showpping-mall/src/page/ProductAll.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
+  const [productList, setProductList] = useState([]);
+  const getProduct=async ()=>{
+    let url = `http://localhost:5000/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data);
+  };
+  useEffect(()=>{
+    getProduct();
+  },[]);
   return (
     <div>
-            
+        <ProductCard/> 
     </div>
   )
 }
